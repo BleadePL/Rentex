05afc0d9331b8f6933d72d48675fef4c6df9c943
Konrad <konrad.wieckiewicz@gmail.com>
2021-11-28 12:07:54 +0100

    Added Login/Logout - deleted unnecessary files

--- web-panel/src/App.js
+++ web-panel/src/App.js
@@ -1,28 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react' 
 import API_Session from "./API_Connector";
+import LoginForm from './components/LoginForm';
 
 const api = new API_Session();
 
+
 function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  // Login
+  //Log out
+
+  const Login = details =>{
+    console.log(details);
+
+    //Praca na bazie
+    // api.login(details.email, details.password, setError("success"), setError("invalid_data"), setError("authorization_err"));
+
+    // if(error == "success"){
+    //   console.log("Logged in");
+    //   setUser({
+    //     name: details.name,
+    //     email : details.email,
+    //     password : details.password
+    //   });
+    // }else {
+    //   console.log(error);
+    // }
+
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");
+      setError("success");
+
+      setUser({
+        name: details.name,
+        email : details.email,
+        password : details.password
+      });
+  
+    }
+    else setError("invalid_data");
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""});
+    setError("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <button onClick={async () => api.login("test", "test", ()=>console.log("Success"), ()=>console.log("Invalid Arguemnt"), () => console.log("Authorisation failed"))}>LOGIN</button>
-          <button onClick={async () => api.logout( ()=>console.log("Success"), ()=>console.log("Failed"))}>LOGOUT</button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {(user.email != "") ? (
+          <div className="welcome">
+            <h2>Welcome, <span>{user.name}</span></h2>
+            <button onClick={Logout}>Logout</button>
+          </div>
+        ): (
+          <LoginForm Login={Login} error={error}/>
+        )}
     </div>
   );
 }
