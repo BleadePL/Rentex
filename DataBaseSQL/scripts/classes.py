021145dbf6d9f3b2c77855bbe39335e32e006f27
JakubMakarewicz <makaron@KOMPUTER>
2022-01-25 12:24:12 +0100

    some small changes

--- DataBaseSQL/scripts/classes.py
+++ DataBaseSQL/scripts/classes.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+import enum
 from re import T
 from sqlite3 import Time
-from sqlalchemy import VARCHAR, BigInteger, CHAR, Column, DECIMAL, ForeignKey, Integer, String, Table, create_engine, text, DateTime
+from sqlalchemy import VARCHAR, Enum, BigInteger, CHAR, Column, DECIMAL, ForeignKey, Integer, String, Table, create_engine, text, DateTime
 from sqlalchemy.dialects.mysql import TIME
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,6 +13,34 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 metadata = Base.metadata
 
+class CarStatusEnum(str, enum.Enum):
+    ACTIVE="ACTIVE"
+    RESERVED="RESERVED"
+    INACTIVE="INACTIVE"
+    SERVICE="SERVICE"
+    INUSE="INUSE"
+    UNKNOWN="UNKNOWN"
+
+class AccountTypeEnum(str, enum.Enum):
+    PERSONAL="PERSONAL"
+    COMPANY="COMPANY"
+    ORGANISATION="ORGANISATION"
+    UNKNOWN="UNKNOWN"
+
+class AccountStatusEnum(str, enum.Enum):
+    ACTIVE="ACTIVE"
+    INACTIVE="INACTIVE"
+    DOCUMENTS="DOCUMENTS"
+    PENDING="PENDING"
+    PAYMENT="PAYMENT"
+    LOCKED="LOCKED"
+    DELETED="DELETED"
+class LocationType(str,enum.Enum):
+    STATION="STATION"
+    CLEAN="CLEAN"
+    SERVICE="SERVICE"
+    SPECIAL_POINT="SPECIAL_POINT"
+    UNKNOWN="UNKNOWN"
 
 class Car(Base):
     __tablename__ = 'Cars'
@@ -30,15 +59,16 @@ class Car(Base):
     currentLocationLong = Column(DECIMAL(11, 8), nullable=False)
     lastUpdateTime = Column(DateTime, nullable=False)
     lastService = Column(DateTime, nullable=False)
-    status = Column(String(3), nullable=False,
+    status = Column(Enum(CarStatusEnum), nullable=False,
                     comment='ACTIVE - aktywny\\nRESERVED - zarezerwowany\\nINACTIVE - wyłączony z użycia\\nSERVICE - Serwisowany\\nINUSE - w uzyciu\\nUNKNOWN - nieznany stan')
     activationCost = Column(String(30), nullable=False)
     kmCost = Column(String(30), nullable=False)
     timeCost = Column(String(30), nullable=False)
     esimNumber = Column(Integer, nullable=False)
     eSimImei = Column(Integer, nullable=False)
-    rentals = relationship("Rental", back_populates="car")
-    services = relationship("Service", back_populates="car")
+
+    rentals = relationship("Rental", back_populates="car", lazy='noload')
+    services = relationship("Service", back_populates="car", lazy='noload')
 
 
 class Client(Base):
@@ -55,40 +85,38 @@ class Client(Base):
     login = Column(String(20), nullable=False)
     password = Column(String(64), nullable=False)
     email = Column(String(50), nullable=False)
-    accountType = Column(String(10), nullable=False,
+    accountType = Column(Enum(AccountTypeEnum), nullable=False,
                          comment='PERSONAL - osoba prywatna\\nCOMPANY - firma\\nORGANISATION - organizacja\\nUNKNOWN - Inne')
     activationCode = Column(Integer)
-    status = Column(String(10), nullable=False,
+    status = Column(Enum(AccountStatusEnum), nullable=False,
                     comment='ACTIVE - Aktywne konto\\nINACTIVE - nieaktwyne konto\\nDOCUMENTS - Brak dokumentow\\nPENDING - wyslano dokumenty, oczekiwanie na potwierdzenie\\nPAYMENT - Brak srodkow na koncie\\nLOCKED - konto zablokowane\\nDELETED - konto usuniete')
 
-    Roles = relationship('Role', secondary='ClientRoles')
-    creditCards = relationship("CreditCard", back_populates="client")
-    rentals = relationship("Rental", back_populates="client")
-    reservations = relationship("Reservation", back_populates="client")
-    services = relationship("Service", back_populates="client")
+    roles = relationship('Role', secondary='ClientRoles', lazy='noload')
+    creditCards = relationship("CreditCard", back_populates="client", lazy='noload')
+    rentals = relationship("Rental", back_populates="client", lazy='noload')
+    reservations = relationship("Reservation", back_populates="client", lazy='noload')
+    services = relationship("Service", back_populates="client", lazy='noload')
 
 class Service(Base):
-     __tablename__ = 'Services'
-     serviceId = Column(Integer, primary_key=True, unique=True)
-     dateStart = Column(DateTime, nullable=False)
-     dateEnd = Column(DateTime)
-     description = Column(String(255))
-     
-     locationId = Column(Integer, ForeignKey('Locations.locationId'))
-     carId = Column(Integer, ForeignKey('Cars.carId'))
-     clientId = Column(Integer, ForeignKey('Clients.clientId'))
-
-     location = relationship("Location", back_populates="services")
-     car = relationship("Car", back_populates="services")
-     client = relationship("Client", back_populates="services") 
-
+    __tablename__ = 'Services'
+    serviceId = Column(Integer, primary_key=True, unique=True)
+    dateStart = Column(DateTime, nullable=False)
+    dateEnd = Column(DateTime)
+    description = Column(String(255))
+    
+    locationId = Column(Integer, ForeignKey('Locations.locationId'))
+    carId = Column(Integer, ForeignKey('Cars.carId'))
+    clientId = Column(Integer, ForeignKey('Clients.clientId'))
+    location = relationship("Location", back_populates="services", lazy='noload')
+    car = relationship("Car", back_populates="services", lazy='noload')
+    client = relationship("Client", back_populates="services", lazy='noload') 
 
 
 class Location(Base):
     __tablename__ = 'Locations'
 
     locationId = Column(Integer, primary_key=True, unique=True)
-    locationType = Column(String(10), nullable=False,
+    locationType = Column(Enum(LocationType), nullable=False,
                           comment='STATION - Stacja benzynowa\\nCLEAN - stacja czyszczenia\\nSERVICE - Serwis\\nSPECIAL_POINT - Punkt specjalny\\nUNKNOWN - nieznany')
     locationName = Column(String(100), nullable=False)
     locationAddress = Column(String(100))
@@ -96,7 +124,7 @@ class Location(Base):
     locationLat = Column(DECIMAL(10, 8), nullable=False)
     locationLong = Column(DECIMAL(11, 8), nullable=False)
     
-    services = relationship("Service", back_populates="location") 
+    services = relationship("Service", back_populates="location", lazy='noload') 
 
 
 class Role(Base):
@@ -118,43 +146,43 @@ class CreditCard(Base):
     __tablename__ = 'CreditCards'
 
     creditCardId = Column(Integer, primary_key=True, unique=True)
-    clientId = Column(ForeignKey('Clients.clientId'), nullable=False, index=True)
     cardNumber = Column(Integer)
     expirationDate = Column(Integer)
     cardHolderName = Column(String(30))
     cardHolderAddress = Column(String(30))
 
-    client = relationship('Client', back_populates="creditCards")
+    clientId = Column(ForeignKey('Clients.clientId'), nullable=False, index=True)
+    client = relationship('Client', back_populates="creditCards", lazy="noload")
 
 
 class Rental(Base):
     __tablename__ = 'Rentals'
 
     rentalId = Column(BigInteger, primary_key=True, unique=True)
-    carId = Column(ForeignKey('Cars.carId'), nullable=False, index=True)
-    clientId = Column(ForeignKey('Clients.clientId'), index=True)
     rentalStart = Column(DateTime, nullable=False)
     rentalEnd = Column(DateTime)
     mileage = Column(Integer, nullable=False)
     cost = Column(String(30))
     ended = Column(Integer, nullable=False)
 
-    car = relationship('Car', back_populates="rentals")
-    client = relationship('Client', back_populates="rentals")
+    carId = Column(ForeignKey('Cars.carId'), nullable=False, index=True)
+    clientId = Column(ForeignKey('Clients.clientId'), index=True)
+    car = relationship('Car', back_populates="rentals", lazy='noload')
+    client = relationship('Client', back_populates="rentals", lazy='noload')
 
 
 class Reservation(Base):
     __tablename__ = 'Reservations'
 
     reservationId = Column(Integer, primary_key=True, unique=True)
-    carId = Column(ForeignKey('Cars.carId'), nullable=False, index=True)
-    clientId = Column(ForeignKey('Clients.clientId'), index=True)
     reservationStart = Column(DateTime, nullable=False)
     reservationEnd = Column(DateTime)
     reservationCost = Column(Integer, nullable=False)
 
-    Car = relationship('Car')
-    client = relationship('Client', back_populates="reservations")
+    clientId = Column(ForeignKey('Clients.clientId'), index=True)
+    carId = Column(ForeignKey('Cars.carId'), nullable=False, index=True)
+    Car = relationship('Car', lazy='noload')
+    client = relationship('Client', back_populates="reservations", lazy='noload')
 
 if __name__ == "__main__":
     url = f'postgresql://postgres:passwd@127.0.0.1:5432'
